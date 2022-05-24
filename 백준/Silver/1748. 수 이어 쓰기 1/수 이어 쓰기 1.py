b5ee4d73da1eb00a07c6df7d274ec87c552d9e67
geun1 <69511382+geun1@users.noreply.github.com>
2022-05-24 20:57:06 +0900

    [Silver III] Title: 수 이어 쓰기 1, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/1748. 수 이어 쓰기 1/수 이어 쓰기 1.py
+++ 백준/Silver/1748. 수 이어 쓰기 1/수 이어 쓰기 1.py
@@ -1,10 +1,2 @@
-n = input()
-lth = len(n)
-result = 0
-if (int(n) < 10):
-    print(n)
-else:
-    result += (int(n)-(10**(lth-1))+1) * lth
-    for i in range(lth-1):
-        result += (9*(10 ** i))*(i+1)
-    print(result)
+n=input()
+print(int(n)*len(n)+(10-10**len(n))//9+len(n)-1)
