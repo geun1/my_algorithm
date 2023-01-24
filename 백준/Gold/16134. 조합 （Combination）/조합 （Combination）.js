a007968d14f25e055b591b99c4cca03854b2193d
gsong <69511382+geun1@users.noreply.github.com>
2023-01-24 16:40:34 +0900

    [Gold I] Title: 조합 (Combination), Time: 120 ms, Memory: 9332 KB -BaekjoonHub

--- 백준/Gold/16134. 조합 （Combination）/조합 （Combination）.js
+++ 백준/Gold/16134. 조합 （Combination）/조합 （Combination）.js
@@ -1,4 +1,4 @@
-//문제: 16134
+//문제: test
 const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 let input = fs.readFileSync(filePath).toString().trim().split("\n");
@@ -6,12 +6,22 @@ let input = fs.readFileSync(filePath).toString().trim().split("\n");
 solution(input);
 function solution(input) {
   const [n, k] = input[0].split(" ").map(Number);
-  const combination = (n, k) => {
-    let result = 1;
-    for (let i = 1; i <= k; i++) {
-      result = (result * (n - (k - i))) / i;
+  function combinationCalculator(n, k) {
+    let C = Array(n + 1);
+    for (let i = 0; i <= n; i++) {
+      C[i] = Array(k + 1).fill(0);
     }
-    return result;
-  };
-  console.log(combination(n, k) % 1000000007);
+    for (let i = 0; i <= n; i++) {
+      for (let j = 0; j <= Math.min(i, k); j++) {
+        if (j === 0 || j === i) {
+          C[i][j] = 1;
+        } else {
+          C[i][j] = C[i - 1][j - 1] + C[i - 1][j];
+        }
+      }
+    }
+    return C[n][k];
+  }
+
+  console.log(combinationCalculator(n, k) % 1000000007);
 }
