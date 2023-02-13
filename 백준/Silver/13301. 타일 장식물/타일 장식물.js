f14aa8e6d802e90d354663816a6f2cddd0730773
gsong <69511382+geun1@users.noreply.github.com>
2023-02-13 14:09:27 +0900

    [Silver V] Title: 타일 장식물, Time: 180 ms, Memory: 9340 KB -BaekjoonHub

---
+++ 백준/Silver/13301. 타일 장식물/타일 장식물.js
@@ -0,0 +1,21 @@
+//문제: 13301
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+let input = fs.readFileSync(filePath).toString().trim().split("\n");
+
+solution(input);
+function solution(input) {
+  // 1 -> 1 1  4
+  // 2 -> 1 2  6
+  // 3 -> 3 2  10
+  // 4 -> 3 5  16
+  // 5 -> 8 5  26
+  // 6 -> 8 13 42
+  let n = +input[0];
+
+  const dp = [4, 6];
+  for (let i = 2; i <= n; i++) {
+    dp[i] = dp[i - 1] + dp[i - 2];
+  }
+  console.log(dp[n - 1]);
+}
