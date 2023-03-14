d893d05b8621345c13a0565011d214c7d029b577
gsong <69511382+geun1@users.noreply.github.com>
2023-03-14 16:10:13 +0900

    [Bronze IV] Title: Equality, Time: 124 ms, Memory: 9344 KB -BaekjoonHub

---
+++ 백준/Bronze/13985. Equality/Equality.js
@@ -0,0 +1,11 @@
+//문제: 13985
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+let input = fs.readFileSync(filePath).toString().trim().split("\n");
+
+solution(input);
+function solution(input) {
+  const arr = input[0].split(" ");
+  if (+arr[0] + +arr[2] === +arr[4]) console.log("YES");
+  else console.log("NO");
+}
