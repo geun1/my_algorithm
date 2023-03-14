14d4f71085863378036bbc4fa9d64a8e6bc79e78
gsong <69511382+geun1@users.noreply.github.com>
2023-03-14 17:05:44 +0900

    [Bronze IV] Title: 폰 노이만과 파리, Time: 136 ms, Memory: 9324 KB -BaekjoonHub

---
+++ 백준/Bronze/14924. 폰 노이만과 파리/폰 노이만과 파리.js
@@ -0,0 +1,10 @@
+//문제: 14924
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+let input = fs.readFileSync(filePath).toString().trim().split("\n");
+
+solution(input);
+function solution(input) {
+  const [a, b, c] = input[0].split(" ").map(Number);
+  console.log((c / (a * 2)) * b);
+}
