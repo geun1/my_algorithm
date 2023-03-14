13389b9f2702c170bd0da3803f346ddf251888a3
gsong <69511382+geun1@users.noreply.github.com>
2023-03-14 14:54:42 +0900

    [Bronze IV] Title: 한글 2, Time: 140 ms, Memory: 9596 KB -BaekjoonHub

---
+++ 백준/Bronze/11283. 한글 2/한글 2.js
@@ -0,0 +1,9 @@
+//문제: 11283
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+let input = fs.readFileSync(filePath).toString().trim().split("\n");
+
+solution(input);
+function solution(input) {
+  console.log(input[0].charCodeAt(0) - 44031);
+}
