b843ef343bd8143d1101e31cfb2a0d00acaa26d5
gsong <69511382+geun1@users.noreply.github.com>
2023-03-14 16:08:45 +0900

    [Bronze IV] Title: 팀 나누기, Time: 176 ms, Memory: 9588 KB -BaekjoonHub

---
+++ 백준/Bronze/13866. 팀 나누기/팀 나누기.js
@@ -0,0 +1,10 @@
+//문제: 13866
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+let input = fs.readFileSync(filePath).toString().trim().split("\n");
+
+solution(input);
+function solution(input) {
+  const [a, b, c, d] = input[0].split(" ").map(Number);
+  console.log(Math.abs(d + a - c - b));
+}
