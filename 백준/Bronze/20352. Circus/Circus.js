c2182bcca906d8f78143ec2f8ae191df3730c7d4
gsong <69511382+geun1@users.noreply.github.com>
2023-03-30 17:39:10 +0900

    [Bronze IV] Title: Circus, Time: 124 ms, Memory: 9588 KB -BaekjoonHub

---
+++ 백준/Bronze/20352. Circus/Circus.js
@@ -0,0 +1,10 @@
+//문제: 20352
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+let input = fs.readFileSync(filePath).toString().trim().split("\n");
+
+solution(input);
+function solution(input) {
+  let w = +input[0];
+  console.log(2 * Math.PI * Math.sqrt(w / Math.PI));
+}
