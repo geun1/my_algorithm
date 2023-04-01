470df22a36ae95c7de6b41a38ba40a5ad1a5c7a3
gsong <69511382+geun1@users.noreply.github.com>
2023-04-01 14:05:35 +0900

    [Bronze IV] Title: 2 番目に大きい整数 (The Second Largest Integer), Time: 124 ms, Memory: 9332 KB -BaekjoonHub

---
+++ 백준/Bronze/20976. 2 番目に大きい整数 （The Second Largest Integer）/2 番目に大きい整数 （The Second Largest Integer）.js
@@ -0,0 +1,14 @@
+//문제: 20976
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+let input = fs.readFileSync(filePath).toString().trim().split("\n");
+
+solution(input);
+function solution(input) {
+  console.log(
+    input[0]
+      .split(" ")
+      .map(Number)
+      .sort((a, b) => b - a)[1]
+  );
+}
