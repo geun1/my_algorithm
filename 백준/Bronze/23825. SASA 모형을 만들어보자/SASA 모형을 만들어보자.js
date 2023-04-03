7c75ae8902b76b1d0c0f9ef143059357f4499b4a
gsong <69511382+geun1@users.noreply.github.com>
2023-04-03 14:07:10 +0900

    [Bronze IV] Title: SASA 모형을 만들어보자, Time: 120 ms, Memory: 9336 KB -BaekjoonHub

---
+++ 백준/Bronze/23825. SASA 모형을 만들어보자/SASA 모형을 만들어보자.js
@@ -0,0 +1,13 @@
+//문제: 23825
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+let input = fs.readFileSync(filePath).toString().trim().split("\n");
+
+solution(input);
+function solution(input) {
+  const [a, b] = input[0]
+    .split(" ")
+    .map(Number)
+    .sort((a, b) => a - b);
+  console.log(parseInt(a / 2));
+}
