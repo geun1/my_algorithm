817fbe15b98c0770b4e915715651e60e422beb48
gsong <69511382+geun1@users.noreply.github.com>
2023-04-11 23:07:58 +0900

    [Bronze IV] Title: 녹색거탑, Time: 136 ms, Memory: 9328 KB -BaekjoonHub

---
+++ 백준/Bronze/24723. 녹색거탑/녹색거탑.js
@@ -0,0 +1,9 @@
+//문제: 24723
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+let input = fs.readFileSync(filePath).toString().trim().split("\n");
+
+solution(input);
+function solution(input) {
+  console.log(Math.pow(2, +input[0]));
+}
