8156a457cbe8a6114626440c02ecb41cdf26635e
gsong <69511382+geun1@users.noreply.github.com>
2023-04-11 23:05:31 +0900

    [Bronze IV] Title: Изгороди, Time: 120 ms, Memory: 9328 KB -BaekjoonHub

---
+++ 백준/Bronze/24623. Изгороди/Изгороди.js
@@ -0,0 +1,9 @@
+//문제: 24623
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+let input = fs.readFileSync(filePath).toString().trim().split("\n");
+
+solution(input);
+function solution(input) {
+  console.log(90);
+}
