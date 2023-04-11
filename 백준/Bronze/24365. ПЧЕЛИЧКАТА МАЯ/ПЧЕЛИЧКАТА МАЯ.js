386d870fcf83ec99f8a806041b51b32110554259
gsong <69511382+geun1@users.noreply.github.com>
2023-04-11 22:00:56 +0900

    [Bronze IV] Title: ПЧЕЛИЧКАТА МАЯ, Time: 124 ms, Memory: 9328 KB -BaekjoonHub

---
+++ 백준/Bronze/24365. ПЧЕЛИЧКАТА МАЯ/ПЧЕЛИЧКАТА МАЯ.js
@@ -0,0 +1,11 @@
+//문제: 24365
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+let input = fs.readFileSync(filePath).toString().trim().split("\n");
+
+solution(input);
+function solution(input) {
+  const [a, b, c] = input[0].split(" ").map(Number);
+  let m = (a + b + c) / 3;
+  console.log(c - a);
+}
