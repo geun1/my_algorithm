e09d6b91ffcfbd9ee5c85a736ab90120676d06e6
gsong <69511382+geun1@users.noreply.github.com>
2023-04-20 17:42:46 +0900

    [Bronze IV] Title: 샤틀버스, Time: 124 ms, Memory: 9336 KB -BaekjoonHub

---
+++ 백준/Bronze/25625. 샤틀버스/샤틀버스.js
@@ -0,0 +1,10 @@
+//문제: 25625
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+let input = fs.readFileSync(filePath).toString().trim().split("\n");
+
+solution(input);
+function solution(input) {
+  const [a, b] = input[0].split(" ").map(Number);
+  a > b ? console.log(a + b) : console.log(b - a);
+}
