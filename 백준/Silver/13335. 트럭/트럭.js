be91557cf182775988947fed695e53d0c897f95a
gsong <69511382+geun1@users.noreply.github.com>
2024-03-28 19:24:11 +0900

    [Silver I] Title: 트럭, Time: 228 ms, Memory: 11852 KB -BaekjoonHub

--- 백준/Silver/13335. 트럭/트럭.js
+++ 백준/Silver/13335. 트럭/트럭.js
@@ -3,9 +3,7 @@ const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 let input = fs.readFileSync(filePath).toString().trim().split("\n");
 
 solution(input);
-function sum(arr) {
-  return arr.reduce((a, b) => a + b, -arr[0]);
-}
+
 function solution(input) {
   const [n, w, L] = input[0].split(" ").map((e) => e * 1);
   // n 개의 트럭 , 다리길이 w, 다리의 최대하중 L
@@ -14,7 +12,7 @@ function solution(input) {
   let answer = 0;
   //   console.log(deque, truck, answer);
   while (true) {
-    if (sum(deque) + truck[0] > L) {
+    if (deque.reduce((a, b) => a + b, -deque[0]) + truck[0] > L) {
       deque.shift();
       deque.push(0);
       answer += 1;
