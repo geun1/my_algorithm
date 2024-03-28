99f11ce1ec5a6cbd48503bdcda370387b5bcc4dd
gsong <69511382+geun1@users.noreply.github.com>
2024-03-28 20:04:17 +0900

    [Silver IV] Title: 문자열 집합, Time: 172 ms, Memory: 31800 KB -BaekjoonHub

--- 백준/Silver/14425. 문자열 집합/문자열 집합.js
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.js
@@ -1,6 +1,6 @@
 //문제: 14425
 //0. 문제 이해 :
-//1. 시간 복잡도 확인:
+//1. 시간 복잡도 확인: 2초 -> 2억연산 n,m <= 1만,
 //2. 적용 알고리즘 확인:
 //3. 맞는 자료구조 확인:
 const fs = require("fs");
@@ -9,20 +9,28 @@ let input = fs.readFileSync(filePath).toString().trim().split("\n");
 
 solution(input);
 function solution(input) {
+    // const [n, m] = input.shift().split(" ").map(Number);
+    // object_n = {};
+    // arr_m = input.slice(n, n + m);
+    // let ans = 0;
+    // for (let i = 0; i < n; i++) {
+    //     object_n[input[i]] = 1;
+    // }
+    // for (let i = 0; i < m; i++) {
+    //     if (object_n[arr_m[i]]) {
+    //         ans++;
+    //     }
+    // }
+    // console.log(ans);
     const [n, m] = input.shift().split(" ").map(Number);
-    object_n = {};
-    arr_m = input.slice(n, n + m);
+    const set = new Set(input.splice(0, n));
+
     let ans = 0;
-    for (let i = 0; i < n; i++) {
-        if (!object_n[input[i]]) object_n[input[i]] = 1;
-        else object_n[input[i]]++;
-    }
-
     for (let i = 0; i < m; i++) {
-        if (object_n[arr_m[i]]) {
-            ans++;
-        }
+        if (set.has(input[i])) ans++;
     }
-
+    // input.forEach((e) => {
+    //     if (set.has(e)) ans++;
+    // });
     console.log(ans);
 }
